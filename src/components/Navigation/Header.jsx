b67ba1e2ff80aec907f72df14410dfc17bc2d424
justallyt <albertjunk2030@gmail.com>
2023-07-19 21:45:36 +0300

    Updates

--- src/components/Navigation/Header.jsx
+++ src/components/Navigation/Header.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom"
-
+import { BsPhoneVibrate } from "react-icons/bs"
 
 const Header = () => {
   return (
@@ -7,8 +7,23 @@ const Header = () => {
             <div className="inner-row">
                       <div className="header-content">
                                 <NavLink to={''}>
-                                           <div className="logo"></div>
+                                           <div className="logo">
+                                                    <h2>Wajibu</h2>
+                                           </div>
                                 </NavLink>
+                                <nav>
+                                         <ul>
+                                                  <li><NavLink to={'/'}>Home</NavLink></li>
+                                                  <li><NavLink to={'/'}>About </NavLink></li>
+                                                  <li><NavLink to={'/'}>Services</NavLink></li>
+                                                  <li><NavLink to={'/'}>Portfolio</NavLink></li>
+                                                  <li><NavLink to={'/'}>Contact Us</NavLink></li>
+                                         </ul>
+                                         <div className="phone-btn">
+                                                    <span><BsPhoneVibrate /></span>
+                                                    <p>+254 712345678</p>
+                                         </div>
+                                </nav>
                       </div>
             </div>
     </header>
