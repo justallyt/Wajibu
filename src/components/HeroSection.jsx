b070e81b5760f9f3bca6f9302e42da8ff4839019
justallyt <albertjunk2030@gmail.com>
2023-07-21 16:35:14 +0300

    Updates

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,25 @@
 import { NavLink } from "react-router-dom"
-
-
+import { PiDotOutlineFill } from 'react-icons/pi'
+import { IoTriangleOutline } from "react-icons/io5"
+import { MdOutlineRectangle } from "react-icons/md"
+import { CgShapeRhombus, CgShapeCircle, CgShapeHalfCircle, CgShapeZigzag } from "react-icons/cg"
+import { FaStarOfLife } from "react-icons/fa"
 const HeroSection = () => {
   return (
     <div className="hero-section">
              <div className="inner-row">
+                        <div className="bg-patterns">
+                                   <div className="bg-patterns-container">
+                                               <span className="circle-dot"><CgShapeCircle /></span>
+                                               <span className="triangle"><IoTriangleOutline /></span>
+                                               <span className="full-dot"><PiDotOutlineFill /></span>
+                                               <span className="semi-circle"><CgShapeHalfCircle /></span>
+                                               <span className="rectangle"><MdOutlineRectangle /></span>
+                                               <div className="rhombus"><CgShapeRhombus /></div>
+                                               <span className="bold-star"><FaStarOfLife /></span>
+                                               <span className="zigzag"><CgShapeZigzag /></span>
+                                   </div>
+                        </div>
                        <div className="hero-section-content">
                                  <div className="hero-texts">
                                            <h3><span></span> Dedicated to digital excellence</h3>
