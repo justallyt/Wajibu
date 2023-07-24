708f9f03f76a899678ac7cb9c926771da986e693
justallyt <albertjunk2030@gmail.com>
2023-07-24 15:29:24 +0300

    Updated Services Section

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -5,7 +5,8 @@ import ecommerce from "../assets/icons/ecommerce.png"
 import webapp from "../assets/icons/webapp.png"
 import seo from "../assets/icons/seo-opt.png"
 import maintenance from "../assets/icons/maintenance.png"
-
+import { HiOutlineArrowRight } from "react-icons/hi"
+import { NavLink } from "react-router-dom"
 const ServicesSection = () => {
   return (
     <div className="services-section">
@@ -17,7 +18,22 @@ const ServicesSection = () => {
                                       </div>
 
                                       <div className="services-section-row">
-                                                   <div className="thin-column"></div>
+                                                   <div className="thin-column">
+                                                               <div className="service-box-moja ml-adjust">
+                                                                           <div className="icon">
+                                                                                     <img src={webdev} alt="Ecommerce Icon" />
+                                                                            </div>
+                                                                           <h2>Website Creation from Scratch</h2>
+                                                                           <NavLink to={'/'}><span><HiOutlineArrowRight /></span></NavLink>
+                                                              </div>
+                                                              <div className="service-box-moja mb-adjust">
+                                                                            <div className="icon">
+                                                                                     <img src={revamp} alt="Ecommerce Icon" />
+                                                                            </div>
+                                                                            <h2>Website Redesign & Revamption</h2>
+                                                                            <NavLink to={'/'}><span><HiOutlineArrowRight /></span></NavLink>
+                                                              </div>
+                                                   </div>
                                                    <div className="wide-column">
                                                                 <div className="service-image-part">
                                                                              <img src={serviceImg} alt="" />
@@ -25,11 +41,37 @@ const ServicesSection = () => {
 
                                                                 <div className="service-constituents">
                                                                               <div className="service-box-moja">
-                                                                                           
+                                                                                           <div className="icon">
+                                                                                                    <img src={ecommerce} alt="Ecommerce Icon" />
+                                                                                           </div>
+                                                                                           <h2>Ecommerce Web Solutions</h2>
+                                                                                           <NavLink to={'/'}><span><HiOutlineArrowRight /></span></NavLink>
+                                                                              </div>
+                                                                              <div className="service-box-moja mb-adjust">
+                                                                                           <div className="icon">
+                                                                                                    <img src={webapp} alt="Ecommerce Icon" />
+                                                                                           </div>
+                                                                                           <h2>Web Applications & MVP Development</h2>
+                                                                                           <NavLink to={'/'}><span><HiOutlineArrowRight /></span></NavLink>
                                                                               </div>
                                                                 </div>
                                                    </div>
-                                                   <div className="thin-column"></div>
+                                                   <div className="thin-column">
+                                                           <div className="service-box-moja">
+                                                                           <div className="icon">
+                                                                                     <img src={seo} alt="Ecommerce Icon" />
+                                                                            </div>
+                                                                           <h2>Search Engine Optimization</h2>
+                                                                           <NavLink to={'/'}><span><HiOutlineArrowRight /></span></NavLink>
+                                                              </div>
+                                                              <div className="service-box-moja mb-adjust">
+                                                                            <div className="icon">
+                                                                                     <img src={maintenance} alt="Ecommerce Icon" />
+                                                                            </div>
+                                                                            <h2>Website Maintenance Services</h2>
+                                                                            <NavLink to={'/'}><span><HiOutlineArrowRight /></span></NavLink>
+                                                              </div>
+                                                   </div>
                                       </div>
                            </div>
               </div>
