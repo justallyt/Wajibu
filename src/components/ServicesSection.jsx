6e960ea91e46f69b2c584864546c45d242e5fc90
justallyt <albertjunk2030@gmail.com>
2023-07-24 14:46:27 +0300

    Updated Services Section

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,3 +1,10 @@
+import serviceImg from "../assets/services1.jpg"
+import webdev from "../assets/icons/webdev.png"
+import revamp from "../assets/icons/revamp.png"
+import ecommerce from "../assets/icons/ecommerce.png"
+import webapp from "../assets/icons/webapp.png"
+import seo from "../assets/icons/seo-opt.png"
+import maintenance from "../assets/icons/maintenance.png"
 
 const ServicesSection = () => {
   return (
@@ -6,7 +13,23 @@ const ServicesSection = () => {
                            <div className="services-section-content">
                                       <div className="intro">
                                                <h3>What We do</h3>
-                                               <h1>Turning your Ideas into Elegant designs and great experiences.</h1>
+                                               <h1>We turn your Ideas into Elegant designs and great experiences.</h1>
+                                      </div>
+
+                                      <div className="services-section-row">
+                                                   <div className="thin-column"></div>
+                                                   <div className="wide-column">
+                                                                <div className="service-image-part">
+                                                                             <img src={serviceImg} alt="" />
+                                                                </div>
+
+                                                                <div className="service-constituents">
+                                                                              <div className="service-box-moja">
+                                                                                           
+                                                                              </div>
+                                                                </div>
+                                                   </div>
+                                                   <div className="thin-column"></div>
                                       </div>
                            </div>
               </div>
