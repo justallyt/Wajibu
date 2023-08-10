ad6ea6a554ba49ba04d9a2481a2bf9712a8ed352
justallyt <albertjunk2030@gmail.com>
2023-08-10 23:55:24 +0300

    Updates

--- src/components/ServicesBody.jsx
+++ src/components/ServicesBody.jsx
@@ -1,4 +1,7 @@
-
+import webdev1 from "../assets/webdev.jpg"
+import webdev2 from "../assets/webdev1.jpg"
+import econ1 from '../assets/ecommerce.jpg'
+import econ2 from '../assets/ecommerce2.jpg'
 const ServicesBody = () => {
   return (
     <div className="services-body-section">
@@ -7,6 +10,39 @@ const ServicesBody = () => {
                                         <div className="body-intro">
                                                    <h2>We offer a Diversity of Skills</h2>
                                                    <p>Our technology-first approach and focus on innovation, powered by our highly skilled workforce, enables us to provide our customers with highly customized digital solutions that facilitates them in gaining a competitive advantage in today&apos;s fast-changing and competitive marketplace.</p>
+                                        </div>
+
+                                        <div className="body-content-row">
+                                                  <div className="service-type-moja">
+                                                             <div className="service-type-image">
+                                                                        <img src={webdev1} alt="" />
+                                                             </div>
+                                                             <div className="service-type-description">
+                                                                      <h2>Web Development</h2>
+                                                                      <p>We specialize in creating custom web solutions tailored to your unique goals and needs. Our team of experienced developers employ industry best practices to build responsive, user-friendly websites and web apps that are effective and efficient while delivering value to your organization.</p>
+                                                                      <p>The tools we use to build first class user experiences that stand out are categorized into either one or both of the following specialities:</p>
+                                                                      <div className="tools-wrapper">
+                                                                                <p>1. <span>Frontend Development</span></p>
+                                                                                <p>Great user interfaces are imperative, therefore incorporating varied layouts, parallax effects and cool modern animations ensures we integrate just the right amount of interaction that keep end users interested.</p>
+                                                                                <p>Under this docket, we use HTML, CSS, TailwindCSS, JavaScript, React, NextJS, GSAP, Framer Motion etc</p>
+
+                                                                                <p>2. <span>Backend Development</span></p>
+                                                                                <p>From powerful and bespoke enterprise business applications to small time features required to effectively run organizations, we get our hands dirty building and integrating APIs, creating and managing databases from the ground up to your precise requirements. </p>
+                                                                                <p>Under this docket, we use PHP, NodeJs(Express), MySQL/MariaDB, MongoDB etc</p>
+                                                                      </div>
+                                                             </div>
+                                                  </div>
+
+                                                  <div className="service-type-ecommerce">
+                                                               <div className="service-type-description">
+                                                                           <h2>Ecommerce Solutions</h2>
+                                                                           <p>Many elements combine in order to create a successful ecommerce platforms for your clients. Brand positioning, UI,UX are essential design elements for a seamless, intuitive shopping cart and checkout process that is required to ensure you maximize your potential on online sales.</p>
+                                                                           <p>We particularly use wordpress CMS  to craft up an ecommerce platform that sells. We also purpose to integrate payment solutions to ensure seamless secure online purchasing by your customers. </p>
+                                                               </div>
+                                                               <div className="ecommerce-images">
+                                                                          <img src={} alt="" />
+                                                               </div>
+                                                  </div>
                                         </div>
                            </div>
                 </div>
